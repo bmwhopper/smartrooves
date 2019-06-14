2bc3dc56b6a869d3fc5e9ac338a6e4692d02c2a9
Luca Iannario <lucaiannario@gmail.com>
2019-06-14 20:30:29 +0100

    updated test API client

--- chaincode/rest/smartrooves-test-API.sh
+++ chaincode/rest/smartrooves-test-API.sh
@@ -3,7 +3,7 @@
 ##Default setting
 CHANNEL_NAME="default"
 CC_NAME="smartrooves_cc"
-CC_VERSION="v4"
+CC_VERSION="v10"
 REST_ADDR="https://DC6CD508163343B6821EDF6A568BF8DB.blockchain.ocp.oraclecloud.com:443/restproxy1"
 USER=""
 PASSWORD=""
@@ -23,40 +23,40 @@ PASSWORD=""
 # echo -n "Enter port of your BCS REST proxy and press [ENTER]: "
 # read -e REST_PORT
 if [[ "$USER" == "" ]]; then
-	#statements
-	echo -n "Enter username of your BCS REST proxy and press [ENTER]: "
-	read -e USER
+  #statements
+  echo -n "Enter username of your BCS REST proxy and press [ENTER]: "
+  read -e USER
 fi
 if [[ "$PASSWORD" == "" ]]; then
-	#statements
-	echo -n "Enter password of your BCS REST proxy and press [ENTER]: "
-    read -s PASSWORD
+  #statements
+  echo -n "Enter password of your BCS REST proxy and press [ENTER]: "
+  read -s PASSWORD
 fi
 
 echo
 
 starttime=$(date +%s)
 
-#check the ressponse
+#check the response
 checkRep(){
-    res=${@:1}
-    if [[  $res = *Failure* ]]; then
-    	if [[ $res = *exists* ]]; then
-    		echo "Things already exists. Continue...."
-    	else
-    		echo "Option Failure, please check the response message and try again."
-    		exit
-    	fi
+  res=${@:1}
+  if [[  $res = *Failure* ]]; then
+    if [[ $res = *exists* ]]; then
+      echo "Things already exists. Continue...."
+    else
+      echo "Request Failure, please check the response message and try again."
+      exit
     fi
+  fi
 
-    if [[ $res = *Success* ]]; then
-    	echo "Option Success, Continue..."
-    fi
+  if [[ $res = *Success* ]]; then
+    echo "Request Success, Continue..."
+  fi
 }
 
 echo "Set Initial State of the Ledger"
 
-echo "Create a new apartment"
+echo "Invocation - create a new apartment"
 echo
 RESPONSE=$(curl -H "Content-type:application/json" -X POST -u $USER:$PASSWORD \
 -d '{"channel":"'"$CHANNEL_NAME"'","chaincode":"'"$CC_NAME"'","method":"initApartment","args":["apt00001", "4", "EastPoint Dublin 3", "15000", "10000", "false", "false", "true", "null", "Gov"],"chaincodeVer":"'"$CC_VERSION"'"}' \
@@ -66,4 +66,52 @@ checkRep $RESPONSE
 echo
 echo
 
+echo "Invocation - create a new tenant"
+echo
+RESPONSE=$(curl -H "Content-type:application/json" -X POST -u $USER:$PASSWORD \
+-d '{"channel":"'"$CHANNEL_NAME"'","chaincode":"'"$CC_NAME"'","method":"initTenant","args":["12234234VA", "James", "Dublin", "05/03/1980", "single", "1", "5500", "1500", "true", "null"],"chaincodeVer":"'"$CC_VERSION"'"}' \
+$REST_ADDR/bcsgw/rest/v1/transaction/invocation);
+echo $RESPONSE
+checkRep $RESPONSE
+echo
+echo
 
+echo "Query - getAvailableApartments"
+echo
+RESPONSE=$(curl -H "Content-type:application/json" -X POST -u $USER:$PASSWORD \
+-d '{"channel":"'"$CHANNEL_NAME"'","chaincode":"'"$CC_NAME"'","method":"getAvailableApartments","args":[],"chaincodeVer":"'"$CC_VERSION"'"}' \
+$REST_ADDR/bcsgw/rest/v1/transaction/query);
+echo $RESPONSE
+checkRep $RESPONSE
+echo
+echo
+
+echo "Query - getAvailableApartments (via querySmartRooves)"
+echo
+RESPONSE=$(curl -H "Content-type:application/json" -X POST -u $USER:$PASSWORD \
+-d '{"channel":"'"$CHANNEL_NAME"'","chaincode":"'"$CC_NAME"'","method":"querySmartRooves","args":["SELECT json_extract(valueJson, '\''$.apartmentId'\'') as apartmentId FROM <STATE> WHERE json_extract(valueJson, '\''$.docType'\'', '\''$.assigned'\'') = '\''[\"apartment\",false]'\''"],"chaincodeVer":"'"$CC_VERSION"'"}' \
+$REST_ADDR/bcsgw/rest/v1/transaction/query);
+echo $RESPONSE
+checkRep $RESPONSE
+echo
+echo
+
+echo "Query - getAvailableTenants"
+echo
+RESPONSE=$(curl -H "Content-type:application/json" -X POST -u $USER:$PASSWORD \
+-d '{"channel":"'"$CHANNEL_NAME"'","chaincode":"'"$CC_NAME"'","method":"getAvailableTenants","args":[],"chaincodeVer":"'"$CC_VERSION"'"}' \
+$REST_ADDR/bcsgw/rest/v1/transaction/query);
+echo $RESPONSE
+checkRep $RESPONSE
+echo
+echo
+
+echo "Query - getAvailableTenants (via querySmartRooves)"
+echo
+RESPONSE=$(curl -H "Content-type:application/json" -X POST -u $USER:$PASSWORD \
+-d '{"channel":"'"$CHANNEL_NAME"'","chaincode":"'"$CC_NAME"'","method":"querySmartRooves","args":["SELECT json_extract(valueJson, '\''$.ppsNumber'\'') as ppsNumber FROM <STATE> WHERE json_extract(valueJson, '\''$.docType'\'', '\''$.apartmentId'\'') = '\''[\"tenant\",\"null\"]'\''"],"chaincodeVer":"'"$CC_VERSION"'"}' \
+$REST_ADDR/bcsgw/rest/v1/transaction/query);
+echo $RESPONSE
+checkRep $RESPONSE
+echo
+echo
