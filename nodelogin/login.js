5090ccde95861d995c923f1aa005bf1f82a49ca5
bmwhopper <brianmathews.mathews8@gmail.com>
2019-06-19 14:36:32 +0100

    Update login.js

--- nodelogin/login.js
+++ nodelogin/login.js
@@ -29,6 +29,9 @@ app.post('/auth', function(request, response) {
 	var password = request.body.password;
 	if (username && password) {
 		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if ( username == "brianmathews" && password == "password"){
+			alert ("Login successfully");
+			}
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
